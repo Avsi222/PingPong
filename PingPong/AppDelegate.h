f1b0b306b8e4f8f0b57a9fb03c5bb788f1cf8555
«Avsi222» <dorogin.arseniy@yandex.ru>
2019-03-10 08:51:31 +0300

    Initial Commit

---
+++ PingPong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PingPong
+//
+//  Created by Арсений Дорогин on 10/03/2019.
+//  Copyright © 2019 Арсений Дорогин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PingPong/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PingPong
+//
+//  Created by Арсений Дорогин on 10/03/2019.
+//  Copyright © 2019 Арсений Дорогин. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
